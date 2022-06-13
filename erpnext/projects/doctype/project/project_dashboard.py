7e1477137ad1653119f42eb0ccccb3b15d9232e9
Vincent T090 <vinhnguyen.t090@gmail.com>
2022-06-13 17:32:51 +0700

    Vincent Project add Mlm event

--- erpnext/projects/doctype/project/project_dashboard.py
+++ erpnext/projects/doctype/project/project_dashboard.py
@@ -10,7 +10,7 @@ def get_data():
 		'transactions': [
 			{
 				'label': _('Project'),
-				'items': ['Task', 'Timesheet', 'Employee Advance', 'Expense Claim', 'Issue' , 'Project Update']
+				'items': ['MLM Event','Task', 'Timesheet', 'Employee Advance', 'Expense Claim', 'Issue' , 'Project Update']
 			},
 			{
 				'label': _('Material'),
