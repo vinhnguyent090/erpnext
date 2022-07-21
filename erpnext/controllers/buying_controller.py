65cedaf4e609a71ce76b8cf6c2ed260378a3a103
Vincent T090 <vinhnguyen.t090@gmail.com>
2022-07-21 12:11:54 +0700

    Vincent fixed link asset

--- erpnext/controllers/buying_controller.py
+++ erpnext/controllers/buying_controller.py
@@ -667,8 +667,8 @@ class BuyingController(StockController, Subcontracting):
 
 							is_plural = "s" if len(created_assets) != 1 else ""
 							messages.append(
-								_("Asset{} {assets_link} created for {}").format(
-									is_plural, frappe.bold(d.item_code), assets_link=assets_link
+								_("Asset{} {} created for {}").format(
+									is_plural, assets_link, frappe.bold(d.item_code)
 								)
 							)
 					else:
