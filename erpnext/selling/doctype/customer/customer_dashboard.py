093e48a46fe89a6ea04c50a03d97b6eb088c87cd
Vincent T090 <vinhnguyen.t090@gmail.com>
2022-10-05 16:39:17 +0700

    Update customer_dashboard.py

--- erpnext/selling/doctype/customer/customer_dashboard.py
+++ erpnext/selling/doctype/customer/customer_dashboard.py
@@ -14,11 +14,12 @@ def get_data():
 			"Opportunity": "party_name",
 			"Bank Account": "party",
 			"Subscription": "party",
+			"Contract": "party_name"
 		},
 		"dynamic_links": {"party_name": ["Customer", "quotation_to"]},
 		"transactions": [
 			{"label": _("Pre Sales"), "items": ["Opportunity", "Quotation"]},
-			{"label": _("Orders"), "items": ["Sales Order", "Delivery Note", "Sales Invoice"]},
+			{"label": _("Orders"), "items": ["Sales Order", "Delivery Note", "Sales Invoice", "Contract"]},
 			{"label": _("Payments"), "items": ["Payment Entry", "Bank Account"]},
 			{
 				"label": _("Support"),
